5d84a850cef746d3e865b1dcb69bb37002eb93dd
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-18 22:05:06 +0600

    blog page complete and added with index file using js dom

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// Method 1
+function goToBlogPage() {
+    // Redirect to the otherpage.html when the button is clicked
+    window.location.href = 'blog-page/blog.html';
+}
+
+// Method 2
+// Get a reference to the button element using its ID
+// const navigateButton = document.getElementById('nav-button');
+
+// // Add a click event listener to the button
+// navigateButton.addEventListener('click', function () {
+//     // Redirect to the otherpage.html when the button is clicked
+//     window.location.href = 'blog-page/blog.html';
+// });
+
